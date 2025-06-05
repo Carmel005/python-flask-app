ed9424e2a444efab89cc6c8d73ebcd0c23e78ab8
Carmel A <157226565+Carmel005@users.noreply.github.com>
2025-06-05 08:19:46 -0600

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ appService = AppService();
 
 @app.route('/')
 def home():
-    return "The Python Application is Working!!!"
+    return "The Python Application is Working and good to go "
 
 
 @app.route('/api/tasks')
